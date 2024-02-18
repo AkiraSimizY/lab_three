106a7f5566c63ba222248daa30fdffaa8c1b96e0
AkiraSimizY <147933205+AkiraSimizY@users.noreply.github.com>
2024-02-19 01:25:55 +0700

    Lab_3

---
+++ Лабораторная_3.py
@@ -0,0 +1,44 @@
+class Book:
+    """ Базовый класс книги. """
+    def __init__(self, name: str, author: str):
+        self._name = name
+        self._author = author
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def author(self):
+        return self._author
+
+    def __str__(self):
+        return f"Книга {self.name}. Автор {self.author}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
+
+
+class PaperBook(Book):
+    def __init__(self, name: str, author: str, pages: int):
+        super().__init__(name, author)
+        self.pages = pages
+
+    def __str__(self):
+        return f"Книга {self.name}. Автор {self.author}. Количество страниц: {self.pages}"
+
+
+class AudioBook(Book):
+    def __init__(self, name: str, author: str, duration: float):
+        super().__init__(name, author)
+        self.duration = duration
+
+    def __str__(self):
+        return f"Книга {self.name}. Автор {self.author}. Длительность: {self.duration} ч."
+
+
+paper_book = PaperBook("Гарри Поттер", "Джоан Роулинг", 400)
+print(paper_book)
+
+audio_book = AudioBook("1984", "Джордж Оруэлл", 9.5)
+print(audio_book)
